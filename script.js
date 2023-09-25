35c54625e626efe5f9e3a2abe5c49257aa083ccd
Vidyasba <116746304+Vidyasba@users.noreply.github.com>
2023-09-25 15:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const btn=document.getElementById('submit');
+const name=document.getElementById('name');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+const Cpassword=document.getElementById('confirm-password');
+console.log("here"+ password.value +" "+Cpassword.value);
+
+btn.addEventListener("click",function(){
+	if(password.value !== Cpassword.value){
+		alert('Passwords do not match');
+	}else{
+		sessionStorage.setItem('name',name);
+		sessionStorage.setItem('email',email);
+		sessionStorage.setItem('password',password);
+
+		alert('Sign up successful!');
+	}
+})
